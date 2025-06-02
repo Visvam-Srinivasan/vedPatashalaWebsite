c63d5de43944504335d4d294516e1650703ab431
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-06-02 21:29:13 +0530

    Update login page, add background image and a two column table for image and text and the form

---
+++ frontend/src/components/login/Login2.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Login.css';
+
+function Login2() {
+
+    return (
+        <div class="wrapper">
+        <div class="container">
+            <div class="left-side">
+            <img src="..\..\assets\login\ganeshaPic.png" alt="Lord Ganesha" />
+            <div class="shloka">
+                śuklāṁ baradharaṁ viṣṇuṁ śaśi varṇaṁ caturbhujam <br />
+                prasanna vadanam dhyāyet sarva vighnopa śāntaye ||<br /><br />
+                yasya dviradha vaktrādyāḥ pāriṣadhya paraśatam <br />
+                vighnam nighnanti satataṁ viśvaksena tamāśraye ||
+            </div>
+            </div>
+            <div class="right-side">
+            <div class="login-box">
+                <h2>Login</h2>
+                <form>
+                <label>Email</label>
+                <input type="email" placeholder="Enter email" required />
+
+                <label>Password</label>
+                <input type="password" placeholder="Enter password" required />
+
+                <label>
+                    <input type="checkbox" /> Show Password
+                </label>
+
+                <button type="submit">SIGN IN</button>
+
+                <div class="links">
+                    <p><a href="#">Forgot Username / Password?</a></p>
+                    <p>Don't have an account? <a href="#">Sign up</a></p>
+                </div>
+                </form>
+            </div>
+            </div>
+        </div>
+        </div>
+
+    )
+}
+
+export default Login2;
+
