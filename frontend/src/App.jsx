4dc7a35e6bc5aea0dfc304bfbd01cff11594e100
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-06-02 21:30:04 +0530

    Update with new login - Login2 component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
 import Login from "./components/login/Login";
+import Login2 from "./components/login/Login2";
 
 function App() {
     return (
         <>
             <Navbar></Navbar>
-            <section className="bg-amber-50 pt-10">
+            <section className="bg-amber-50">
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<Login2 />} />
             </Routes>
             </section>
         </>
