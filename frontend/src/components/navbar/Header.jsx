589d7e4fb7a7b0f5c9f73caa5dfd42d1680a1766
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-06-03 21:54:17 +0530

    Change Navbar to Header and update gradient

---
+++ frontend/src/components/navbar/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../tailwind.css';
+import './header.css';
+
+function Header() {
+    return (
+        <>
+        <nav className="navbar">
+            <div className="mx-auto">
+                <h1 className="text-center text-5xl font-cinzel font-medium">
+                    VED PATASHALA
+                </h1>
+            </div>
+        </nav>
+        </>
+    )
+
+}
+export default Header;
