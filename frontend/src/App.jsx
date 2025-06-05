6b667975fc260d6f744e43d9a9cb1d668b84496a
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-06-05 14:24:01 +0530

    Update routes to include Home and AdminNavbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './global.css';
 import Login from "./components/login/Login";
 import Header from "./components/header/Header";
+import Home from "./components/Home";
+import AdminNavbar from "./components/users/admin/AdminNavbar";
 
 function App() {
     return (
         <>
             <Header></Header>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/admin" element={<Login />} />
             </Routes>
         </>
     )
