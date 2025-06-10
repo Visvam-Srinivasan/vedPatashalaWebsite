c47f70c29cbe363bcd51b97313da1ad465604d1a
Visvam Srinivasan <yp2.visvam@gmail.com>
2025-06-10 21:05:19 +0530

    Update routes for admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,13 @@ import Login from "./components/login/Login";
 import Header from "./components/header/Header";
 import Home from "./components/Home";
 import AdminNavbar from "./components/users/admin/AdminNavbar";
+import AdminDashboard from "./components/users/admin/AdminDashboard";
+import AdminWrapper from "./components/users/admin/AdminWrapper";
+import AdminManageRoles from "./components/users/admin/AdminManageRoles";
+import AdminManageUsers from "./components/users/admin/AdminManageUsers";
+import AdminManageCourses from "./components/users/admin/AdminManageCourses";
+
+
 
 function App() {
     return (
@@ -14,7 +21,13 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/admin" element={<AdminNavbar />} />
+                <Route path="/admin" element={<AdminWrapper />}>
+                    <Route path="dashboard" element={<AdminDashboard />} />
+                    <Route path="manageUsers" element={<AdminManageUsers />} />
+                    <Route path="manageRoles" element={<AdminManageRoles />} />
+                    <Route path="manageCourses" element={<AdminManageCourses />} />
+                </Route>
+
             </Routes>
         </>
     )
